a42d88dbca9d4cba70a973b3e77f606f4be533e3
jmJang00 <50986275+jmJang00@users.noreply.github.com>
2021-06-21 06:01:06 +0000

    wildcard

--- wildcard.cpp
+++ wildcard.cpp
@@ -8,34 +8,29 @@ using namespace std;
 int cache[101][101];
 string W, S;
 
-bool solve(int w, int s) {
-    int ret = 0;
+int solve(int w, int s) {
+    int& ret = cache[w][s];
     
-    if (cache[w][s] != -1) {
-        return cache[w][s];
+    if (ret != -1) {
+        return ret;
     }
 
-    while (w < W.length() && s < S.length()) {
-        if (W[w] == '*')
-            break;
-        else if (W[w] == '?' || W[w] == S[s]) {
-            ++w;
-            ++s;
+    while (w < W.length() && s < S.length() && (W[w] == '?' || W[w] == S[s])) {
+        ++w;
+        ++s;
+    }
+
+    if (w == W.length())
+        return ret = (s == S.length());
+
+    if (W[w] == '*') {
+        for (int i = 0; i + s <= S.length(); ++i) {
+            if (solve(w+1, s+i) == 1)
+                return ret = 1;
         }
-        else {
-            return false;
-        }
     }
 
-    if (w == W.length() && s < S.length())
-        return false;
-
-    for (int i = 0; i + s < S.length(); ++i) {
-        if (w + 1 < W.length() && W[w+1] == S[s])
-            solve(w, s); // TODO
-    }
-
-    return ret;
+    return ret = 0;
 }
 
 int main()
@@ -48,13 +43,18 @@ int main()
         
         int numTarget;
         cin >> numTarget;
-        
-        memset(cache, -1, sizeof(int) * 101 * 101);
+        vector<string> targets;
         
         for (int i = 0; i < numTarget; ++i) {
+            memset(cache, -1, sizeof(int) * 101 * 101);
             cin >> S;
-            cout << solve(0, 0) << endl;
+            if (solve(0, 0))
+                targets.push_back(S);
         }
+
+        sort(targets.begin(), targets.end());
+        
+        for (auto p: targets) cout << p << endl;
     }
     
     return 0;
